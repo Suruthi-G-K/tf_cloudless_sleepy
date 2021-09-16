e338fa267819e1e1b7a85d164fc38d3d56db0a4f
Suruthi Ganesan Kalavathy <suruthiganesankalavathy@Suruthis-MacBook-Pro.local>
2021-09-16 17:36:07 +0530

    command to fetch env var

--- main.tf
+++ main.tf
@@ -1,11 +1,4 @@
 # Create an arbitrary local resource
-data "template_file" "test" {
-  template = "Hello, I am a template. My sample_var value = $${sample_var}"
-
-  vars = {
-    sample_var = var.sample_var
-  }
-}
 
 # This is just to make sure that ibm provider is instantiated
 /* data "ibm_schematics_workspace" "test" {
@@ -19,7 +12,7 @@ resource "null_resource" "sleep" {
   }
 
   provisioner "local-exec" {
-    command = "sleep ${var.sleepy_time}"
+    command = "jq -n env"
   }
 }
 
